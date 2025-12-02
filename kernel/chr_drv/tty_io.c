82803349e165d99ae53f181d3aad584ac25cf14f
yuji <yujibuzailai_sun@outlook.com>
2025-12-02 15:24:10 +0800

    进入内核

---
+++ kernel/chr_drv/tty_io.c
@@ -0,0 +1,5 @@
+#include "../../include/linux/tty.h"
+
+void tty_init() {
+    con_init();
+}
---
+++ kernel/main.c
@@ -0,0 +1,12 @@
+#define __LIBRARY__
+
+void main(void)
+{
+    tty_init();
+    __asm__("int $0x80 \n\r"::);
+    __asm__ __volatile__(
+        "loop:\n\r"
+        "jmp loop"
+        ::
+    );
+}
